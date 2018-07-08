99d9a2752565a4d204241249981a7ad0f851914e
Joseph Parker <jparker010190@gmail.com>
2018-07-08 19:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ const fareDoubler = function(fare){
 const fareTripler = function(fare){
   return fare * 3;
 }
-function selectDifferentDrivers(drivers){
-  return drivers
+function selectDifferentDrivers(drivers, driver){
+  return driver
 }
