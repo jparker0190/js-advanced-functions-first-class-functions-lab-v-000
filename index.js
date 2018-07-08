2ba55b0ff579c653a8b1e8d78536b1c0ef64d952
Joseph Parker <jparker010190@gmail.com>
2018-07-08 19:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
-const fun = function returnFirstTwoDrivers(drivers) {};
+const fun = function returnFirstTwoDrivers(drivers) {var j = drivers.slice(0, 2)
+return j};
 function returnFirstTwoDrivers(drivers){
   var j = drivers.slice(0, 2)
   return j
